aac502a08d851a92e7d9ef79b316d3717b1808ef
jacquebento <cae22001@byui.edu>
2023-02-01 16:43:34 -0500

    w5 learning activities

---
+++ week05/scripts/weather.js
@@ -0,0 +1,20 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const townName = "Trier";
+const myKey = "de5f1660d9f3688e5be36daf091ea9db"
+
+const myURL = `//api.openweathermap.org/data/2.5/weather?q=${townName}&appid=${myKey}&units=imperial`;
+
+fetch(myURL)
+    .then((response) => response.json())
+    .then((data) => displayData(data));
+
+    function displayData(data){
+        weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        weatherIcon.alt = data.weather[0].main;
+        captionDesc.innerHTML = data.weather[0].description;
+        currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    }
