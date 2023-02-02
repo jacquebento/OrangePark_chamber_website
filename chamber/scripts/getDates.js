c6b5a9d5b1f7bc084283894e700ea123deb79bb0
Jacqueline Bento <104387400+jacquebento@users.noreply.github.com>
2023-02-02 14:40:30 -0500

    Update getDates.js

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -4,4 +4,3 @@ y.innerHTML = date;
 
 let modify = document.lastModified;
 document.querySelector("#lastModified").innerHTML = `Last modified: ${modify}`;
-document.querySelector(".date").value = modify;
